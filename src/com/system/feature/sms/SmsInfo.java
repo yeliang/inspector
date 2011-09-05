c858b314373f9695322e07aabbf49d55b06a74c6
richardroky <richardroky@gmail.com>
2011-09-05 07:35:43 +0000

    update

---
+++ src/com/system/feature/sms/SmsInfo.java
@@ -0,0 +1,42 @@
+package com.system.feature.sms;
+
+import java.util.Date;
+
+import android.content.Context;
+import android.content.res.Resources;
+import com.system.R;
+
+public class SmsInfo
+{
+	public String SendPersonName;
+	public String phoneNumber;
+	public String smsbody;
+	public Date date;
+	public SmsType type;
+	
+	public String toString(Context context)
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(date.toLocaleString() + "\t");
+		sb.append(typeToString(context, type) + "\t");
+		if (SendPersonName != null && SendPersonName.length() > 0) sb.append(SendPersonName);
+		if (phoneNumber != null && phoneNumber.length() > 0) sb.append("(" + phoneNumber + "):\t");
+		sb.append(smsbody);
+		
+		return sb.toString();
+	}
+	
+	private String typeToString(Context context, SmsType type)
+	{
+		if (type == SmsType.RECEIVED)
+			return context.getResources().getString(R.string.sms_received);
+		if (type == SmsType.SENT)
+			return context.getResources().getString(R.string.sms_sent);
+		else
+			return context.getResources().getString(R.string.sms_unknown);
+	}
+
+}
+
+
