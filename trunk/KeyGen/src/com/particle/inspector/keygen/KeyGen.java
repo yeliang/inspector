3679ca7b1f38a0bed02da4789d29ed94edfcc423
richardroky@gmail.com <richardroky@gmail.com@b3a3b4a4-ea58-8126-2e7c-08f2ffd412ec>
2011-10-14 05:34:17 +0000

    enhance Keygen to make it configurable

--- trunk/KeyGen/src/com/particle/inspector/keygen/KeyGen.java
+++ trunk/KeyGen/src/com/particle/inspector/keygen/KeyGen.java
@@ -6,11 +6,13 @@ import java.util.Random;
 
 import com.particle.inspector.keygen.R;
 import com.particle.inspector.common.util.AesCryptor;
+import com.particle.inspector.common.util.license.LICENSE_TYPE;
 import com.particle.inspector.common.util.mail.GMailSenderEx;
 import com.particle.inspector.common.util.SysUtils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -23,7 +25,6 @@ import android.widget.TextView;
 
 public class KeyGen extends Activity 
 {   
-	private static int KEYS_NUMBER = 100;
 	private static int KEY_LENGTH = 12; // *** It should be even number and less than 32 ***
 	
 	protected static final String LOGTAG = KeyGen.class.toString();
@@ -31,6 +32,7 @@ public class KeyGen extends Activity
 	private Button btnGenerate100full;
 	private Button btnGenerate100part;
 	private Button btnGenerate100super;
+	private Button btnSetting;
 	private TextView fullLicense;
 	private TextView partLicense;
 	private TextView superLicense;
@@ -53,13 +55,16 @@ public class KeyGen extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        btnGenerate = (Button)findViewById(R.id.btn_generate);
-        btnGenerate100full = (Button)findViewById(R.id.btn_generate100full);
-        btnGenerate100part = (Button)findViewById(R.id.btn_generate100part);
-        btnGenerate100super = (Button)findViewById(R.id.btn_generate100super);
-        fullLicense = (TextView)findViewById(R.id.txtFullLicense);
-        partLicense = (TextView)findViewById(R.id.txtPartLicense);
-        superLicense = (TextView)findViewById(R.id.txtSuperLicense);
+        context = getApplicationContext();
+        initUI(context);
+        
+        btnSetting.setOnClickListener(new OnClickListener() {
+        	public void onClick(View v)
+        	{
+        		Intent intent = new Intent().setClass(getApplicationContext(), GlobalPrefActivity.class);
+        		startActivityForResult(intent, R.layout.main);
+        	}
+        });
         
         btnGenerate.setOnClickListener(new OnClickListener() {
         	public void onClick(View v)
@@ -97,13 +102,18 @@ public class KeyGen extends Activity
     			new Thread(new Runnable(){
     				public void run() {
     					mHandler.sendEmptyMessageDelayed(DISABLE_GENERATE100FULL_BTN, 0);
+    					
+    					int keyCount = GlobalPrefActivity.getKeyCount(context) > 0 ? GlobalPrefActivity.getKeyCount(context) : GlobalPrefActivity.DEFAULT_KEYS_NUMBER;
+    					String senderMailAddr = GlobalPrefActivity.getSenderMail(context);
+    					String senderPwd = GlobalPrefActivity.getSenderPwd(context);
+    					String receiverMailAddr = GlobalPrefActivity.getReceiverMail(context);
     	    			
     	        		StringBuilder sb = new StringBuilder();
     	        		sb.append("No.\t\t" + "Full Key" + SysUtils.NEWLINE);
     	        		sb.append("-----------------------------------" + SysUtils.NEWLINE);
     	        		
     	        		try {
-    	        			for (int i = 0; i < KEYS_NUMBER; i++) {
+    	        			for (int i = 0; i < keyCount; i++) {
     	        				long seed = Math.abs((new Random()).nextLong());
     	        				String hex = Long.toHexString(seed);
     	        				String clearText = hex.substring(0, KEY_LENGTH/2).toUpperCase(); 
@@ -114,10 +124,10 @@ public class KeyGen extends Activity
     	        			}
     	        			
     	        			// Send mail
-    	        			GMailSenderEx gmailSender = new GMailSenderEx("richardroky@gmail.com", "yel636636");
+    	        			GMailSenderEx gmailSender = new GMailSenderEx(senderMailAddr, senderPwd);
     	                    gmailSender.setFrom("system@gmail.com");
-    	                    gmailSender.setTo(new String[] {"ylssww@126.com"});
-    	                    gmailSender.setSubject("100 full keys - " + (new SimpleDateFormat("yyyyMMdd")).format(new Date()));
+    	                    gmailSender.setTo(new String[] {receiverMailAddr});
+    	                    gmailSender.setSubject(String.valueOf(keyCount) + " full keys - " + (new SimpleDateFormat("yyyyMMdd")).format(new Date()));
     	                    gmailSender.setBody(sb.toString());
     	                    
     	                    if (gmailSender.send()) {
@@ -154,13 +164,18 @@ public class KeyGen extends Activity
     			new Thread(new Runnable(){
     				public void run() {
     					mHandler.sendEmptyMessageDelayed(DISABLE_GENERATE100PART_BTN, 0);
+    					
+    					int keyCount = GlobalPrefActivity.getKeyCount(context) > 0 ? GlobalPrefActivity.getKeyCount(context) : GlobalPrefActivity.DEFAULT_KEYS_NUMBER;
+    					String senderMailAddr = GlobalPrefActivity.getSenderMail(context);
+    					String senderPwd = GlobalPrefActivity.getSenderPwd(context);
+    					String receiverMailAddr = GlobalPrefActivity.getReceiverMail(context);
     	    			
     	        		StringBuilder sb = new StringBuilder();
     	        		sb.append("No.\t\t" + "Part Key" + SysUtils.NEWLINE);
     	        		sb.append("-----------------------------------" + SysUtils.NEWLINE);
     	        		
     	        		try {
-    	        			for (int i = 0; i < KEYS_NUMBER; i++) {
+    	        			for (int i = 0; i < keyCount; i++) {
     	        				long seed = Math.abs((new Random()).nextLong());
     	        				String hex = Long.toHexString(seed);
     	        				String clearText = hex.substring(0, KEY_LENGTH/2).toUpperCase(); 
@@ -170,10 +185,10 @@ public class KeyGen extends Activity
     	        			}
     	        			
     	        			// Send mail
-    	        			GMailSenderEx gmailSender = new GMailSenderEx("richardroky@gmail.com", "yel636636");
+    	        			GMailSenderEx gmailSender = new GMailSenderEx(senderMailAddr, senderPwd);
     	                    gmailSender.setFrom("system@gmail.com");
-    	                    gmailSender.setTo(new String[] {"ylssww@126.com"});
-    	                    gmailSender.setSubject("100 part keys - " + (new SimpleDateFormat("yyyyMMdd")).format(new Date()));
+    	                    gmailSender.setTo(new String[] {receiverMailAddr});
+    	                    gmailSender.setSubject(String.valueOf(keyCount) + " part keys - " + (new SimpleDateFormat("yyyyMMdd")).format(new Date()));
     	                    gmailSender.setBody(sb.toString());
     	                    
     	                    if (gmailSender.send()) {
@@ -210,13 +225,18 @@ public class KeyGen extends Activity
     			new Thread(new Runnable(){
     				public void run() {
     					mHandler.sendEmptyMessageDelayed(DISABLE_GENERATE100SUPER_BTN, 0);
+    					
+    					int keyCount = GlobalPrefActivity.getKeyCount(context) > 0 ? GlobalPrefActivity.getKeyCount(context) : GlobalPrefActivity.DEFAULT_KEYS_NUMBER;
+    					String senderMailAddr = GlobalPrefActivity.getSenderMail(context);
+    					String senderPwd = GlobalPrefActivity.getSenderPwd(context);
+    					String receiverMailAddr = GlobalPrefActivity.getReceiverMail(context);
     	    			
     	        		StringBuilder sb = new StringBuilder();
     	        		sb.append("No.\t\t" + "Super Key" + SysUtils.NEWLINE);
     	        		sb.append("-----------------------------------" + SysUtils.NEWLINE);
     	        		
     	        		try {
-    	        			for (int i = 0; i < KEYS_NUMBER; i++) {
+    	        			for (int i = 0; i < keyCount; i++) {
     	        				long seed = Math.abs((new Random()).nextLong());
     	        				String hex = Long.toHexString(seed);
     	        				String clearText = hex.substring(0, KEY_LENGTH/2).toUpperCase(); 
@@ -226,10 +246,10 @@ public class KeyGen extends Activity
     	        			}
     	        			
     	        			// Send mail
-    	        			GMailSenderEx gmailSender = new GMailSenderEx("richardroky@gmail.com", "yel636636");
+    	        			GMailSenderEx gmailSender = new GMailSenderEx(senderMailAddr, senderPwd);
     	                    gmailSender.setFrom("system@gmail.com");
-    	                    gmailSender.setTo(new String[] {"ylssww@126.com"});
-    	                    gmailSender.setSubject("100 super keys - " + (new SimpleDateFormat("yyyyMMdd")).format(new Date()));
+    	                    gmailSender.setTo(new String[] {receiverMailAddr});
+    	                    gmailSender.setSubject(String.valueOf(keyCount) + " super keys - " + (new SimpleDateFormat("yyyyMMdd")).format(new Date()));
     	                    gmailSender.setBody(sb.toString());
     	                    
     	                    if (gmailSender.send()) {
@@ -253,7 +273,45 @@ public class KeyGen extends Activity
         
     }
     
-    // Update UI 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+    {
+    	if (resultCode == RESULT_OK) 
+    	{
+            if (GlobalPrefActivity.getKeyCount(context) > 0 &&
+                GlobalPrefActivity.getSenderMail(context).length() > 0 &&
+                GlobalPrefActivity.getSenderPwd(context).length() > 0 &&
+                GlobalPrefActivity.getReceiverMail(context).length() > 0)
+            {
+              	btnGenerate100full.setEnabled(true);
+                btnGenerate100part.setEnabled(true);
+                btnGenerate100super.setEnabled(true);
+            }
+    	}
+    }
+    
+    private void initUI(Context context) {
+    	btnGenerate = (Button)findViewById(R.id.btn_generate);
+        btnGenerate100full = (Button)findViewById(R.id.btn_generate100full);
+        btnGenerate100part = (Button)findViewById(R.id.btn_generate100part);
+        btnGenerate100super = (Button)findViewById(R.id.btn_generate100super);
+        btnSetting =  (Button)findViewById(R.id.btn_setting);
+        fullLicense = (TextView)findViewById(R.id.txtFullLicense);
+        partLicense = (TextView)findViewById(R.id.txtPartLicense);
+        superLicense = (TextView)findViewById(R.id.txtSuperLicense);
+        
+        if (GlobalPrefActivity.getKeyCount(context) <= 0 ||
+            GlobalPrefActivity.getSenderMail(context).length() == 0 ||
+            GlobalPrefActivity.getSenderPwd(context).length() == 0 ||
+            GlobalPrefActivity.getReceiverMail(context).length() == 0)
+        {
+        	btnGenerate100full.setEnabled(false);
+            btnGenerate100part.setEnabled(false);
+            btnGenerate100super.setEnabled(false);
+        }
+	}
+
+	// Update UI 
     private Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
