b8bc29bde66c9274fc5d18c31d3e69c1e9a0898b
richardroky@gmail.com <richardroky@gmail.com@b3a3b4a4-ea58-8126-2e7c-08f2ffd412ec>
2011-11-17 09:20:32 +0000

    update

--- trunk/Inspector/src/system/service/BootReceiver.java
+++ trunk/Inspector/src/system/service/BootReceiver.java
@@ -16,7 +16,7 @@ import android.telephony.TelephonyManager;
  */
 public class BootReceiver extends BroadcastReceiver {
 	
-	private TelephonyManager mTelephonyMgr;
+	private static final String LOGTAG = "BootReceiver";
 	
 	@Override
 	public void onReceive(Context context, Intent bootintent) {
@@ -24,36 +24,53 @@ public class BootReceiver extends BroadcastReceiver {
 		
 		if (bootintent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) 
 		{
-			mTelephonyMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-			
 			// Start service
 			Intent mServiceIntent = new Intent(context, BootService.class);
 			context.startService(mServiceIntent);
+			
+			SysUtils.threadSleep(3000, LOGTAG);
+		
+			// ------------------------------------------------------------------
+			// Start to check if changed SIM card
+			
+			TelephonyManager mTelephonyMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			//int simState = mTelephonyMgr.getSimState();
+			
+			String simSerialNumber = mTelephonyMgr.getSimSerialNumber();
+			String hostSimSerialNumber = ConfigCtrl.getSimSerialNum(context);
+			
+			// I found that cannot get serial number of ChinaTelecom SIM card (getSimSerialNumber() return null), but ChinaUnicom can.
+			if ((simSerialNumber == null || simSerialNumber.length() <= 0) &&
+				(hostSimSerialNumber == null || hostSimSerialNumber.length() <= 0)) 
+			{
+				return;
+			}
+			
+			boolean isFirstRun = ConfigCtrl.getSimFirstRun(context);
+			if (isFirstRun) {
+				ConfigCtrl.setSimFirstRun(context, false);
+				if (simSerialNumber != null && simSerialNumber.length() > 0) {
+					ConfigCtrl.setSimSerialNum(context, simSerialNumber);
+				}
+			} else {
+				if ((simSerialNumber != null && hostSimSerialNumber != null && !simSerialNumber.equalsIgnoreCase(hostSimSerialNumber)) ||
+					(simSerialNumber == null && hostSimSerialNumber != null) ||
+					(simSerialNumber != null && hostSimSerialNumber == null))
+				{
+					// Send SMS since the SIM card has changed
+					String recvPhoneNum = GlobalPrefActivity.getReceiverPhoneNum(context);
+					if (recvPhoneNum != null && recvPhoneNum.length() > 0) 
+					{
+						String strContent = String.format(context.getResources().getString(R.string.msg_changed_sim), ConfigCtrl.getSelfName(context));
+						boolean ret = SmsCtrl.sendSms(recvPhoneNum, strContent);
+						if (ret) {
+							ConfigCtrl.setSimSerialNum(context, simSerialNumber == null ? "" : simSerialNumber);
+						}
+					}
+				}
+			}
+			
+		
 		}
-		
-		mTelephonyMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		if (mTelephonyMgr != null && mTelephonyMgr.getSimState() == TelephonyManager.SIM_STATE_READY) 
-        {
-            String simSerialNumber = mTelephonyMgr.getSimSerialNumber();
-            
-            boolean isFirstRun = ConfigCtrl.getSimFirstRun(context);
-            if (isFirstRun) {
-            	ConfigCtrl.setSimFirstRun(context, false);
-                ConfigCtrl.setSimSerialNum(context, simSerialNumber);
-            } else {
-                String hostSimSerialNumber = ConfigCtrl.getSimSerialNum(context);
-                if (!simSerialNumber.equalsIgnoreCase(hostSimSerialNumber)) 
-                {
-                    // Send SMS since the SIM card has changed
-                	String recvPhoneNum = GlobalPrefActivity.getReceiverPhoneNum(context);
-                	if (recvPhoneNum != null && recvPhoneNum.length() > 0) 
-                	{
-                		String strContent = String.format(context.getResources().getString(R.string.msg_changed_sim), ConfigCtrl.getSelfName(context));
-                		SmsCtrl.sendSms(recvPhoneNum, strContent);
-                	}
-                }
-            }
-        }
-		
 	}
 }
